c97955f6c188471cd22a78aef9142d758c66b5d7
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-19 01:04:23 +0700

    Updates

---
+++ precache-manifest.180f4927b4c51d68ccda6c38d2cc3ffc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437f070033343138e70774b70fc13f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3955baae4126617167c",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8211cce544cedf41f06b",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "3ffa09e240e1a082bd36",
+    "url": "/static/css/main.66262938.chunk.css"
+  },
+  {
+    "revision": "111d2ce3b7841af0f08b",
+    "url": "/static/js/2.1b763e0f.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.1b763e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3955baae4126617167c",
+    "url": "/static/js/3.a1ef4ada.chunk.js"
+  },
+  {
+    "revision": "8211cce544cedf41f06b",
+    "url": "/static/js/4.fd75fed4.chunk.js"
+  },
+  {
+    "revision": "3ffa09e240e1a082bd36",
+    "url": "/static/js/main.75a34bb7.chunk.js"
+  },
+  {
+    "revision": "4751baeba576f59ad916",
+    "url": "/static/js/runtime-main.90527db6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "fa05441bb4b70b0e84f86cdb27d02c65",
+    "url": "/static/media/dicoding.fa05441b.png"
+  },
+  {
+    "revision": "f5f28c3abc3393afd1958886f801dd1c",
+    "url": "/static/media/labkomp.f5f28c3a.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "2e435ebac44f88611bd0b803dcab051a",
+    "url": "/static/media/neskar.2e435eba.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
